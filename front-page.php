4c053be07a43a3ba859453d41a6486c48520b215
Talbote <odolinski@MacBook-Pro-de-odolinski.local>
2018-02-04 22:21:07 +0100

    front-page.php -  SECTION 2  L4ST ARTICLE

--- front-page.php
+++ front-page.php
@@ -43,3 +43,61 @@
         </div>
     </section>
     <!-- SECTION 1 IMG / TEXT  / WYZ  FRONTPAGE close -->
+
+    <!-- SECTION 2  L4ST ARTICLE  FRONTPAGE begin -->
+    <section id="section-project" class="no-padding">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="latest-projects clearfix">
+                        <div class="latest-projects-intro">
+                            <h2 class="box-title"><?php _e('Derniers articles', 'traductor') ?></h2>
+                            <div class="tiny-border-light"></div>
+                            <p><?php _e('Pellentesque gravida diam orci, vitae venenatis est eleifend sed. Proin non pretium
+                                    turpis', 'traductor') ?></p>
+                        </div>
+
+                        <div class="latest-projects-wrapper">
+                            <div id="latest-projects-items" class="latest-projects-items">
+
+
+                                <?php
+                                $recentPosts = new WP_Query();
+                                $recentPosts->query('showposts=6');
+                                ?>
+
+                                <?php while
+
+                                ($recentPosts->have_posts()) : $recentPosts->the_post();
+                                    ?>
+
+                                    <div class="item">
+                                        <img src="<?php the_field('article_section2_acf_image') ?>" alt=""
+                                             class="img-responsive">
+                                        <div class="project-details">
+                                            <p class="folio-title"><a
+                                                    href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+                                            </p>
+
+                                            <p class="folio-cate"><i class="fa fa-tag"></i><a
+                                                    href="<?php the_permalink(); ?>"><?php the_category('/'); ?></a>
+                                            </p>
+
+                                            <div class="folio-buttons">
+                                                <a href="<?php the_field('article_section2_acf_image') ?>"
+                                                   title="Plan For Your Bussiness"
+                                                   class="folio"><i class="fa fa-search"></i></a>
+                                                <a href="<?php the_permalink() ?>"><i class="fa fa-link"></i></a>
+                                            </div>
+                                        </div>
+                                    </div>
+                                <?php endwhile; ?>
+                                <?php wp_reset_postdata(); ?>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- SECTION 2  L4ST ARTICLE  FRONTPAGE close -->
