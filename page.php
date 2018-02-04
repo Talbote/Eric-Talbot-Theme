c08d441dea3543416e7f3b903a413128ddfa1c67
Talbote <odolinski@MacBook-Pro-de-odolinski.local>
2018-02-04 22:26:06 +0100

    page.php APROPOS - SECTION 1 TITRE WYZ IMG + VIDEO

---
+++ page.php
@@ -0,0 +1,41 @@
+<?php get_header(); ?>
+
+
+    <div id="content" class="no-padding">
+        
+        <!-- SECTION 1 TITRE WYZ IMG APROPOS + VIDEO open -->
+        <section id="" style="background: url(<?php the_post_thumbnail_url(); ?>)">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-8 col-md-offset-2">
+                        <?php while (have_posts()): the_post(); ?>
+                            <div class="about-text-intro text-center">
+                                <h2><?php the_title(); ?></h2>
+                                <p><?php the_content(); ?></p>
+                            </div>
+                        <?php endwhile; ?>
+
+
+                        <div class="box-intro-video">
+                            <div id="overlay-video" class="overlay-video-intro">
+                                <?php $iframe = get_field('video');
+
+                                preg_match('/src="(.+?)\?/',$iframe, $matches);
+                                $src = $matches[1];
+                                ?>
+                                <img alt="" src="<?php the_field('apropos_section1_iconplay') ?>" class="img-responsive"/>
+                                <a href="<?php echo $src ?>?autoplay=1" class="btn-intro-video"><i class="fa fa-play"></i></a>
+                            </div>
+                            <div id="thevideo" style="display:none">
+                                <iframe id="someFrame" width="750" height="422" src="" frameborder="0" allowfullscreen></iframe>
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!-- SECTION 1 TITRE WYZ IMG APROPOS + VIDEO close -->
+
+
+
