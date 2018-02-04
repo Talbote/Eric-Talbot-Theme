d853474ef84154ae8e95080653f5de678b98646b
Talbote <odolinski@MacBook-Pro-de-odolinski.local>
2018-02-04 22:38:26 +0100

    single.php ARTICLE - SECTION 3 TITRE CATEG AUTHOR DATE WYZ  + WIDGET

--- single.php
+++ single.php
@@ -30,3 +30,75 @@
                             <img src="<?php the_field('article_section2_acf_image') ?>" class="img-responsive">
                         </div>
                         <!-- SECTION 2 ACF IMAGE  close -->
+
+                        <!-- SECTION 3 TITRE CATEG AUTHOR DATE WYZ + WIDGET   open -->
+    <div class="post-content">
+        <div class="post-title">
+            <h1><?php the_title() ?></h1>
+        </div>
+
+        <div class="post-metadata">
+                                        <span class="posted-on">
+                                            <i class="fa fa-clock-o"></i>
+                                            <?php echo get_the_date(); ?>
+                                        </span>
+            <span class="byline">
+                                            <i class="fa fa-user"></i>
+                                            <a href="<?php the_permalink(); ?>"><?php the_author(); ?></a>
+                                        </span>
+            <span class="cat-links">
+                                            <i class="fa fa-folder-open"></i>
+                                            <a href="<?php the_permalink(); ?>"><?php the_category('/') ?></a>
+                                        </span>
+        </div>
+        <div class="hr"></div>
+
+        <div class="post-entry">
+            <p><?php the_content() ?></p>
+        </div>
+
+
+
+        <div class="clearfix"></div>
+    </div>
+<?php endwhile; ?>
+    </article>
+    <!-- post close -->
+    </div>
+
+
+    </div>
+    <div class="col-md-3">
+        <div class="main-sidebar">
+            <aside class="widget widget_text">
+                <h3 class="widget-title">About</h3>
+                <div class="tiny-border"></div>
+                <div class="textwidget">
+                    <p>
+
+                        <?php dynamic_sidebar('text-bloc-1') ?>
+
+                    </p>
+                </div>
+            </aside>
+            <aside class="widget widget_categories">
+                <h3 class="widget-title">Categories</h3>
+                <div class="tiny-border"></div>
+                <?php
+                $tabs = array(
+                    'menu' => 'menu sidebar',
+                    'container' => 'ul',
+                    'menu_class' => 'cat-item',
+                );
+                wp_nav_menu($tabs); ?>
+
+            </aside>
+        </div>
+    </div>
+    </div>
+    </div>
+    </div>
+
+    <!-- SECTION 3 TITRE CATEG AUTHOR DATE WYZ  + WIDGET  close -->
+
+<?php get_footer(); ?>
